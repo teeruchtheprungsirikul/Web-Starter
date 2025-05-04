ae38f32481c43a6039eeac6125dcc696ccbdfc31
teeruchtheprungsirikul <teeruch_th@hotmail.com>
2025-05-05 04:19:19 +0700

    basic JS

--- main.js
+++ main.js
@@ -1,1 +1,43 @@
-prompt('ชื่อของฉัน')
+/*
+object function
+*/
+
+let students = [
+    {
+    name : 'New',
+    score : 50,
+    grade : 'A'
+    },
+    {
+    name : 'test',
+    score : 60,
+    grade : 'C'
+    },
+    {
+        name : 'jj',
+        score : 65,
+        grade : 'D'
+        }
+]
+
+let student = students.find((s) => {
+    if (s.name == 'test') {
+        return true
+    }
+})
+
+// let doublescore_student = students.map((s) => {
+//     s.score = s.score * 2
+//     return s
+// })
+
+let highScore_student = students.filter((s) => {
+    if (s.score >= 60){
+        return true
+    }
+})
+//console.log('student',student)
+
+//console.log('doublescore_student',doublescore_student)
+
+console.log('highScore_student: ', highScore_student)
